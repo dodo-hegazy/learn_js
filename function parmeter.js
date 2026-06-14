2c19529a3c0c3b988b16c403cfffb0ebc44d7366
dodo-hegazy <mohamedhegazyece@gmail.com>
2026-06-14 21:42:20 +0300

    function parameters

--- function parmeter.js
+++ function parmeter.js
@@ -5,13 +5,17 @@
   - Old Strategies [Condition + Logical Or]
   - ES6 Method
 */
+// default function parameters باختصار هو ان احط قيمة افتراضية للبراميتر في حاله عدم تمرير قيمه له
 
-function sayHello(username = "Unknown", age = "Unknown") {
-  // if (age === undefined) {
-  //   age = "Unknown";
-  // }
-  // age = age || "Unknown";
+//  age = age || "Unknown";   يعني لو age غير معرفه او قيمتها undefined او not a number
+
+function sayHello(username , age = "Unknown") {
+ if (age  === undefined) {
+  age = "unknown";
+ }
+ 
+
   return `Hello ${username} Your Age Is ${age}`;
 }
 
-console.log(sayHello());
+console.log(sayHello("Dareen"));   // Hello Dareen Your Age Is Unknown
